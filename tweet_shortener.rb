5ad5f040fee67ab0acf4d56f51fea5d44334edbb
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-26 02:15:32 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,3 +31,10 @@ def bulk_tweet_shortener(tweets)
   end
 end 
 
+def shortened_tweet_truncator(tweet)
+  if  word_substituter(tweet).length > 140
+    word_substituter(tweet)[0...136] + '...'
+  else
+    tweet
+  end
+end 
