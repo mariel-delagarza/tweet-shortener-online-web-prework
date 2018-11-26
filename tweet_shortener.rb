daf32631f8d1730f415631808f87cba30431f4f7
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-26 02:11:32 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,14 +2,26 @@
 
 def dictionary
   dictionary = {
-    "hello" => "hi"
-    "to, two, too" => "2"
-    "for, four" => "4"
-    "be" => "b"
-    "you" => "u"
-    "at" => "@"
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
     "and" => "&"
   }
+end 
   
 def word_substituter(tweet)
-  if tweet.contain 
+  tweet.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else 
+      word 
+    end 
+  end.join(" ")
+end 
+
